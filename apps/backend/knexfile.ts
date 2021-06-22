490350c43b3fac30643fbe476e851b612d2304d7
ChromeGG <kontakt.tkaczyk@gmail.com>
2021-06-22 22:20:27 +0200

    init

---
+++ apps/backend/knexfile.ts
@@ -0,0 +1,23 @@
+import 'dotenv/config';
+import * as Knex from 'knex';
+import { knexSnakeCaseMappers } from 'objection';
+
+module.exports = {
+  client: 'pg',
+  connection: {
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+  },
+  migrations: {
+    directory: './src/database/migrations',
+    // stub: './src/database/migration.stub',
+  },
+  // seeds: {
+  //   directory: './src/database/seeds',
+  //   stub: './src/database/seed.stub',
+  // },
+  ...knexSnakeCaseMappers(),
+} as Knex.Config;
--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,26 +1,10 @@
-import { ConfigModule } from '@nestjs/config';
-
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from './users/users.module';
-
-import { config as typeOrmConfig } from './config/ormconfig';
-
-const ENV = process.env.NODE_ENV;
+import { TagsModule } from './tags/tags.module';
+import { ThemesModule } from './themes/themes.module';
+import { NotesModule } from './notes/notes.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: ENV === 'production' ? '.env' : `.env.${ENV}`,
-    }),
-    TypeOrmModule.forRootAsync(typeOrmConfig),
-    UsersModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TagsModule, ThemesModule, NotesModule, DatabaseModule],
 })
-export class AppModule {}
+export class ApplicationModule {}
