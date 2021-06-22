1fc9d4f07e51416ffa339d5a690dbe5b6d34df28
ChromeGG <kontakt.tkaczyk@gmail.com>
2021-06-22 22:44:12 +0200

    Fix app.module

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,10 +1,22 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { TagsModule } from './tags/tags.module';
 import { ThemesModule } from './themes/themes.module';
 import { NotesModule } from './notes/notes.module';
 import { DatabaseModule } from './database/database.module';
 
+const ENV = process.env.NODE_ENV;
+
 @Module({
-  imports: [TagsModule, ThemesModule, NotesModule, DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ENV === 'production' ? '.env' : `.env.${ENV}`,
+    }),
+    TagsModule,
+    ThemesModule,
+    NotesModule,
+    DatabaseModule,
+  ],
 })
 export class ApplicationModule {}
